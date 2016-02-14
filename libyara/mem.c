27ac16de53803a161405d289daa66b05d4b8b4ac
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2016-02-13 17:03:40 -0800

    Trimming fat and adapting for LC environment (UserMode compiling with DDK).

--- libyara/mem.c
+++ libyara/mem.c
@@ -21,6 +21,14 @@ limitations under the License.
 #include <windows.h>
 #include <string.h>
 
+// NO STRNLEN in DDK
+size_t strnlen( const char *str, size_t maxlen )
+{
+    size_t i;
+    for( i = 0; i < maxlen && str[ i ]; i++ );
+    return i;
+}
+
 #include <yara/error.h>
 
 static HANDLE hHeap;
